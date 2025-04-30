ed05bebcbe2d1ce67cf89fabf712e52b1091f7f2
nikevdp <86020479+nikevdp@users.noreply.github.com>
2025-04-30 20:02:27 -0300

    Create MisFunciones.hs

---
+++ MisFunciones.hs
@@ -0,0 +1,9 @@
+module MisFunciones where
+
+import Test.Hunit
+import MisFunciones
+
+fibonacci :: Int->Int
+fibonacci x
+  | x == 0 || x == 1 = x
+  | otherwise = fibonacci(x - 1) + fibonacci(x - 1)
